70c7b79f7f2edf82e004433f0ff80ce0526be10e
btbuffer <jimohsikiru20@gmail.com>
2024-09-10 13:06:16 +0100

    Remove old folder and contents

--- JavaScript-Frontend/13-book.js
+++
@@ -1,7 +0,0 @@
-const book = {
-  title: "Eloquent JavaScript",
-  description: "A modern introduction to programming",
-  numberOfPages: 463,
-  author: "Marijn Haverbeke",
-  reading: true,
-};
--- JavaScript-Frontend/6-oddEvenNumber.js
+++
@@ -1,4 +0,0 @@
-const number = 4;
-
-if (number % 2 === 0) console.log("Number " + number + " is even");
-else console.log("Number " + number + " is odd");

bcb4be2261ed467665397871f3cad767d0e03c45
btbuffer <jimohsikiru20@gmail.com>
2024-09-11 16:51:25 +0100

    Delete src folder

--- JavaScript/js-challenge/src/10-filter.js
+++
@@ -1,8 +0,0 @@
-export default function filterNegative(arr) {
-  const resArr = [];
-
-  for (let idx = 0; idx < arr.length; idx++) {
-    arr[idx] >= 0 ? resArr.push(arr[idx]) : "";
-  }
-  return resArr;
-}
--- JavaScript/js-challenge/src/2-convertLength.js
+++
@@ -1,4 +0,0 @@
-export default function convertCmtoM(from) {
-  const result = from * 0.01;
-  return `${from}cm ≡ ${result}m`;
-}

2f72710051a669ab260cbb7de839ea87cdc49f60
btbuffer <jimohsikiru20@gmail.com>
2024-10-14 11:05:18 +0100

    Add area of a circle program, Task 4

---
+++ java/Task/AreaOfCircle4.java
@@ -0,0 +1,13 @@
+package Task;
+
+public class AreaOfCircle4
+{
+    public static void main(String[] a)
+    {
+        // Area = PIE * radius * radius;
+        float radius = 12.7f;
+        float area = (float) (Math.PI * radius * radius);
+
+        System.out.println("Area = " + area +'m');
+    }
+}

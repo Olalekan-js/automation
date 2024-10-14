9a476c0ce125e8bb7c309caddea148f39faa1ad6
btbuffer <jimohsikiru20@gmail.com>
2024-10-14 11:01:17 +0100

    Add multiple lines program first commit

---
+++ java/Task/MultipleLines1.java
@@ -0,0 +1,11 @@
+package Task;
+
+class MultipleLines
+{
+    public static void main(String[] a)
+    {
+        System.out.println("Hello, Testers!");
+        System.out.println("Welcome to Java programming language course.");
+        System.out.println("Java is a strongly typed Object Oriented Programming (OOP) language.");
+    }
+}

e2ab14cb45d2d7db2f2e1771684ccca4bdb6af09
btbuffer <jimohsikiru20@gmail.com>
2024-10-20 01:07:55 +0100

    Add method overload concept

---
+++ java/Task/Overloading16A.java
@@ -0,0 +1,27 @@
+package Task;
+
+class Student
+{
+    public void enrollStudent(String name)
+    {
+        System.out.println("Successfully registered, " + name);
+    }
+
+    public void enrollStudent(String name, int age)
+    {
+        System.out.println("Successfully registered, " + name);
+    }
+
+    public void enrollStudent(String name, int age, double fee)
+    {
+        System.out.println("Successfully registered, " + name);
+    }
+}
+
+public class Overloading16A
+{
+    public static void main(String[] a)
+    {
+        // code implementation
+    }
+}
